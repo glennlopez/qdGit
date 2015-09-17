e3079c8d85616ae94198db4467204501773cc375
glennlopez <glennlopez@gmail.com>
2015-09-17 14:05:26 -0500

    ls

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
-#if [ "$?" == 0 ]; then
-	echo 1 > itworked
-	#rm pull.py and push.py
-	#wget pull.py and push.py from github
-	#
-#else
-#	clear
-#	echo 'ERROR: Cannot update. Check your network connection.'
-#fi
+wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
+if [ "$?" == 0 ]; then
+	cd ..
+	rm -f pull.py; rm -f push.py
+	wget --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/push.py
+	wget --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/pull.py
+	chmod +x pull.py;chmod +x push.py
+
+#check for proper connection
+#rm pull.py and push.py
+#wget pull.py and push.py from github
+
+else
+	clear
+	echo 'ERROR: Cannot update. Check your network connection.'
+fi
