52eac9c08ffdefeca34bc87d69498fcc100ce4a5
glennlopez <glennlopez@gmail.com>
2015-09-17 13:52:41 -0500

    update

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 #wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
 #if [ "$?" == 0 ]; then
 	echo 0 > itworked
+	cd ..
+	echo 0 > thisalsoworked
 #check for proper connection
 #rm pull.py and push.py
 #wget pull.py and push.py from github
