a398d176f6a35e92e669fa7b5e226dc67a089811
glennlopez <glennlopez@gmail.com>
2015-09-17 14:07:56 -0500

    update

--- update.sh
+++ update.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
-#if [ "$?" == 0 ]; then
-	echo 0 > itworked
+wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/version
+if [ "$?" == 0 ]; then
 	cd ..
-	echo 0 > poopingwork
+	rm -f pull.py; rm -f push.py
+	wget --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/push.py
+	wget --quiet https://raw.githubusercontent.com/glennlopez/qdGit/core/pull.py
+	chmod +x pull.py;chmod +x push.py
+
 #check for proper connection
 #rm pull.py and push.py
 #wget pull.py and push.py from github
-#
-#else
-#	clear
-#	echo 'ERROR: Cannot update. Check your network connection.'
-#fi
+
+else
+	clear
+	echo 'ERROR: Cannot update. Check your network connection.'
+fi
