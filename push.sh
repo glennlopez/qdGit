0d08f60ba3b28ca3a3d3c72ab8e5894c4c295e33
glennlopez <glennlopez@gmail.com>
2015-09-18 10:42:47 -0500

    update

--- push.sh
+++ push.sh
@@ -21,7 +21,7 @@ Cya='\e[0;36m';     BCya='\e[1;36m';    UCya='\e[4;36m';    ICya='\e[0;96m';    
 Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    BIWhi='\e[1;97m';   On_Whi='\e[47m';    On_IWhi='\e[0;107m';
 
 # Error Codes:
-	error503=$(echo '${Yel}ERROR: FAILED TO FETCH${Whi}')
+	error503=$(echo "${Yel}ERROR: FAILED TO FETCH${Whi}")
 	error404=$(echo 'ERROR: CANNOT \ESTABLISH NETWORK CONNECTION')
 
 # Check network before fetching version number
@@ -65,7 +65,7 @@ function auto_update(){
 
 # Push changes to repo
 function push(){
-	echo "${Yel}ERROR: FAILED TO FETCH${Whi}"
+	echo $error503
 	echo -e "${BICya}blue ${Red}red ${RCol}etc...."
 }
 
