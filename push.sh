c6970a1a78feec4d7c67c2232bb6c741f48b718e
glennlopez <glennlopez@gmail.com>
2015-09-17 20:57:13 -0700

    update

---
+++ push.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##########################
+#version = 0.8
+#debug = 0
+##########################
+
+function script_update(){
+	# check network connection
+	wget --spider --quiet https://raw.githubusercontent.com/glennlopez/qdGit/development/push.sh
+	if [ "$?" == 0 ]; then
+
+		# local script
+		awk '{ if ($1 ~ /#version/) print local $3}' push.sh > tmp
+		loc_ver=$(<tmp)
+		mv push.sh old.push.sh
+
+		# remote script
+		wget --quiet https://raw.githubusercontent.com/glennlopez/qdGit/development/push.sh
+		awk '{ if ($1 ~ /#version/) print local $3}' push.sh > tmp
+		rem_ver=$(<tmp)
+
+		# compare versions for update
+		if [[ $loc_ver < $rem_ver ]]; then
+			rm -f old.push.sh
+			echo 'Script was updated.'
+		else
+			echo 'Local version is up to date.'
+			rm -f push.sh
+			mv old.push.sh push.sh
+		fi
+
+		chmod +x push.sh
+		rm -f tmp
+	else
+		echo 'Skipping update: Cannot establish connection to remote git repository.'
+	fi
+}
+
+script_update
+echo
+echo 'script complete'
